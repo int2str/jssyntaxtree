0dcc7dbce1f31e3b5a1af3a4b29faca3eb67cd52
Mei Eisenbach <mei.eisenbach@gmail.com>
2019-08-11 13:24:51 -0700

    Modify subsequent tip rotations to be sequential

--- tip.js
+++ tip.js
@@ -1,21 +1,15 @@
 // jsSyntaxTree - A syntax tree graph generator
 // (c)2019 Andre Eisenbach <andre@ironcreek.net>
 
-let last_tip_idx = 0;
+const tips = [
+  'Click on the syntax tree image to download a copy.',
+  'Add manual subscripts to nodes using an underscore, &quot;_&quot;.<br /><strong>Example:</strong> [N_s Dogs]',
+  'jsSyntaxTree works offline, instantly updates and handles unicode fonts.',
+];
+
+let tip_idx = Math.floor(Math.random() * tips.length);
 
 function rotateTip(){
-  let label = '<strong>Tip:</strong> '
-  let tips = [
-    'Click on the syntax tree image to download a copy.',
-    'Add manual subscripts to nodes using an underscore, &quot;_&quot;.<br /><strong>Example:</strong> [N_s Dogs]',
-    'jsSyntaxTree works offline, instantly updates and handles unicode fonts.',
-  ];
-
-  let i = last_tip_idx;
-  while (i == last_tip_idx) {
-    i = Math.floor(Math.random() * tips.length);
-  }
-  last_tip_idx = i;
-
-  document.getElementById('tip').innerHTML = label + tips[i];
+  document.getElementById('tip').innerHTML = '<strong>Tip:</strong> ' +
+    tips[tip_idx++ % tips.length];
 }
