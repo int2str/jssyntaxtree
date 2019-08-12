765d92d94fdd0ac421816fff0568daaf161857d9
Andre Eisenbach <github@4ae.us>
2019-08-12 10:22:28 -0700

    Two more tips

--- tip.js
+++ tip.js
@@ -3,13 +3,15 @@
 
 const tips = [
   'Click on the syntax tree image to download a copy.',
-  'Add manual subscripts to nodes using an underscore, &quot;_&quot;.<br /><strong>Example:</strong> [N_s Dogs]',
+  'Add manual subscripts to nodes using an underscore, &quot;_&quot;.<br />Example: [N_s Dogs]',
   'jsSyntaxTree works offline, instantly updates and handles unicode fonts.',
+  'You can right-click the image and copy &amp; paste the graph into your document editor.',
+  'The graph will updarte automatically once a matching number of brackets is detected.',
 ];
 
 let tip_idx = Math.floor(Math.random() * tips.length);
 
-function rotateTip(){
-  document.getElementById('tip').innerHTML = '<strong>Tip:</strong> ' +
-    tips[tip_idx++ % tips.length];
+function rotateTip() {
+  document.getElementById('tip').innerHTML =
+      '<strong>Tip:</strong> ' + tips[tip_idx++ % tips.length];
 }
