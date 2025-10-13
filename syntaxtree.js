18802725300fab4065d70ac1824a1fe2afca4580
Enrique Lopez <lopezenr2005@gmail.com>
2025-10-13 10:53:40 -0700

    Added phrase highlighing to the textarea
    This will result in phrases with an unmatched closing bracket
    to not be highlighted, but phrases with both brackets will be highlighted.
    
    I created this feature for my professor as she mentioned
    the brackts are hard to see for her (she has a visual impairment)
    
    There have been slight edits to index.html and default.css,
    as well as a new file highlight.js which contains
    the logic behind the highlight feature.
    
    Edited by: Andre Eisenbach <git@4ae.us>

--- syntaxtree.js
+++ syntaxtree.js
@@ -1,32 +1,35 @@
 // jsSyntaxTree - A syntax tree graph generator
 // (c)2019 Andre Eisenbach <andre@ironcreek.net>
 
-'use strict';
+"use strict";
 
-const VERSION = 'v1.2';
+const VERSION = "v1.2";
 
-import Tree from './tree.js';
-import rotateTip from './tip.js';
+import Tree from "./tree.js";
+import rotateTip from "./tip.js";
 
-import * as Parser from './parser.js';
-import * as Tokenizer from './tokenizer.js';
+import * as Highlight from "./highlight.js";
+import * as Parser from "./parser.js";
+import * as Tokenizer from "./tokenizer.js";
 
 const tree = new Tree();
 
 window.onload = () => {
   registerServiceWorker();
 
-  e('version').innerHTML = VERSION;
-  tree.setCanvas(e('canvas'));
+  e("version").innerHTML = VERSION;
+  tree.setCanvas(e("canvas"));
   registerCallbacks();
 
-  const query = decodeURI(window.location.search).replace('?', '');
-  if (query != null && query.length > 2) e('code').value = query;
+  const query = decodeURI(window.location.search).replace("?", "");
+  if (query != null && query.length > 2) e("code").value = query;
 
   update();
 
   rotateTip();
   setInterval(rotateTip, 30 * 1000);
+
+  Highlight.setup(e("code"), e("highlight"));
 };
 
 function e(id) {
@@ -34,60 +37,68 @@ function e(id) {
 }
 
 function registerServiceWorker() {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('syntaxtree_worker.js').then(
-      (registration) => { console.info('Service worker registered.'); },
-      (error) => { console.warn('Unable to register service worker.'); }
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("syntaxtree_worker.js").then(
+      (registration) => {
+        console.info("Service worker registered.");
+      },
+      (error) => {
+        console.warn("Unable to register service worker.");
+      },
     );
   } else {
-    console.info('Service workers not supported.');
+    console.info("Service workers not supported.");
   }
 }
 
 function registerCallbacks() {
-  e('code').oninput = update;
+  e("code").oninput = update;
 
-  e('font').onchange = () => {
-    tree.setFont(e('font').value);
+  e("font").onchange = () => {
+    tree.setFont(e("font").value);
     update();
   };
 
-  e('fontsize').onchange = () => {
-    tree.setFontsize(e('fontsize').value);
+  e("fontsize").onchange = () => {
+    tree.setFontsize(e("fontsize").value);
     update();
   };
 
-  e('triangles').onchange = () => {
-    tree.setTriangles(e('triangles').checked);
+  e("triangles").onchange = () => {
+    tree.setTriangles(e("triangles").checked);
     update();
   };
 
-  e('nodecolor').onchange = () => {
-    tree.setColor(e('nodecolor').checked);
+  e("nodecolor").onchange = () => {
+    tree.setColor(e("nodecolor").checked);
     update();
   };
 
-  e('autosub').onchange = () => {
-    tree.setSubscript(e('autosub').checked);
+  e("autosub").onchange = () => {
+    tree.setSubscript(e("autosub").checked);
     update();
   };
 
-  e('align').onchange = () => {
-    tree.setAlignment(parseInt(e('align').value, 10));
+  e("align").onchange = () => {
+    tree.setAlignment(parseInt(e("align").value, 10));
     update();
   };
 
-  e('spacing').oninput = () => {
-    tree.setSpacing(parseFloat(e('spacing').value / 100));
+  e("highlighting").onchange = () => {
+    Highlight.enable(e("code"), e("highlight"), e("highlighting").checked);
+  };
+
+  e("spacing").oninput = () => {
+    tree.setSpacing(parseFloat(e("spacing").value / 100));
     update();
   };
 
-  e('canvas').onclick = () => tree.download();
+  e("canvas").onclick = () => tree.download();
 }
 
 function update() {
-  const phrase = e('code').value;
-  e('parse-error').innerHTML = '';
+  const phrase = e("code").value;
+  e("parse-error").innerHTML = "";
 
   try {
     const tokens = Tokenizer.tokenize(phrase);
@@ -96,18 +107,20 @@ function update() {
     const syntax_tree = Parser.parse(tokens);
     tree.draw(syntax_tree);
   } catch (err) {
-    e('parse-error').innerHTML = err;
+    e("parse-error").innerHTML = err;
   }
 }
 
 function validateTokens(tokens) {
-  if (tokens.length < 3) throw 'Phrase too short';
-  if (tokens[0].type != Tokenizer.TokenType.BRACKET_OPEN ||
-      tokens[tokens.length - 1].type != Tokenizer.TokenType.BRACKET_CLOSE)
-    throw 'Phrase must start with [ and end with ]';
+  if (tokens.length < 3) throw "Phrase too short";
+  if (
+    tokens[0].type != Tokenizer.TokenType.BRACKET_OPEN ||
+    tokens[tokens.length - 1].type != Tokenizer.TokenType.BRACKET_CLOSE
+  )
+    throw "Phrase must start with [ and end with ]";
   const brackets = countOpenBrackets(tokens);
-  if (brackets > 0) throw brackets + ' bracket(s) open [';
-  if (brackets < 0) throw Math.abs(brackets) + ' too many closed bracket(s) ]';
+  if (brackets > 0) throw brackets + " bracket(s) open [";
+  if (brackets < 0) throw Math.abs(brackets) + " too many closed bracket(s) ]";
   return null;
 }
 
--- tip.js
+++ tip.js
@@ -1,26 +1,26 @@
 // jsSyntaxTree - A syntax tree graph generator
 // (c)2019 Andre Eisenbach <andre@ironcreek.net>
 
-'use strict';
+"use strict";
 
 const tips = [
-  'Click on the syntax tree image to download a copy.',
-  'jsSyntaxTree works offline, instantly updates and handles unicode fonts.',
-  'You can right-click the image and copy &amp; paste the graph into your document editor.',
-  'The graph will update automatically once a matching number of brackets is detected.',
-  'Add manual subscripts to nodes using an underscore character.<br />' +
-      'Example: <a href="?[N_s%20Dogs]">[N_s Dogs]</a>',
-  'Add manual superscript to nodes using the ^ character.<br />' +
-      'Example: <a href="?[N^s%20Cats]">[N^s Cats]</a>',
-  'You can add spaces to nodes by putting them inside double quotes.<br />' +
-      'Example: <a href="?[&quot;Main%20clause&quot;%20[S][V][O]]">[&quot;Main clause&quot; [S][V][O]]</a>',
-  'Add arrows to a node by using an -&gt;, &lt- or &lt;&gt; arrow followed by column number.<br />' +
-      'Example: <a href="?[A%20[B%20C][D%20E][F%20G%20->1]]">[A [B C][D E][F G ->1]]</a>'
+  "Click on the syntax tree image to download a copy.",
+  "jsSyntaxTree works offline, instantly updates and handles unicode fonts.",
+  "You can right-click the image and copy &amp; paste the graph into your document editor.",
+  "The graph will update automatically once a matching number of brackets is detected.",
+  "Add manual subscripts to nodes using an underscore character.<br />" +
+    'Example: <a href="?[N_s%20Dogs]">[N_s Dogs]</a>',
+  "Add manual superscript to nodes using the ^ character.<br />" +
+    'Example: <a href="?[N^s%20Cats]">[N^s Cats]</a>',
+  "You can add spaces to nodes by putting them inside double quotes.<br />" +
+    'Example: <a href="?[&quot;Main%20clause&quot;%20[S][V][O]]">[&quot;Main clause&quot; [S][V][O]]</a>',
+  "Add arrows to a node by using an -&gt;, &lt- or &lt;&gt; arrow followed by column number.<br />" +
+    'Example: <a href="?[A%20[B%20C][D%20E][F%20G%20->1]]">[A [B C][D E][F G ->1]]</a>',
 ];
 
 let tip_idx = Math.floor(Math.random() * tips.length);
 
 export default function rotateTip() {
-  document.getElementById('tip').innerHTML =
-      '<strong>Tip:</strong> ' + tips[tip_idx++ % tips.length];
+  document.getElementById("tip").innerHTML =
+    "<strong>Tip:</strong> " + tips[tip_idx++ % tips.length];
 }
